a7b15cd7629d5f71642c51c382ddcc8bbfcdadca
Dwayne Bent <dbb@dbb.io>
2013-08-24 14:06:24 -0400

    Add extension method to center rect on screen

--- Source/Ketchup/Extensions/MiscExtensions.cs
+++ Source/Ketchup/Extensions/MiscExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Ketchup.Api;
+using UnityEngine;
 
 namespace Ketchup.Extensions
 {
@@ -7,6 +8,15 @@ namespace Ketchup.Extensions
         public static bool IsHalted(this IDcpu16 dcpu16)
         {
             return dcpu16.Memory[dcpu16.PC] == 0x8382; // ADD PC, -1
+        }
+
+        public static Rect CenteredOnScreen(this Rect rect)
+        {
+            return new Rect(rect)
+            {
+                x = (Screen.width / 2f) - (rect.width / 2),
+                y = (Screen.height / 2f) - (rect.height / 2)
+            };
         }
     }
 }
