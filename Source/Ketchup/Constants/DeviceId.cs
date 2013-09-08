a1943a64ba4f8f014189558b4c673d000bb05bc0
Dwayne Bent <dbb@dbb.io>
2013-09-08 09:38:47 -0400

    Add Sped3 class

--- Source/Ketchup/Constants/DeviceId.cs
+++ Source/Ketchup/Constants/DeviceId.cs
@@ -7,5 +7,6 @@
         GenericKeyboard     = 0x30cf7406,
         Lem1802Monitor      = 0x7349f615,
         M35FdFloppyDrive    = 0x4fd524c5,
+        Sped3               = 0x42babf3c,
     }
 }
---
+++ Source/Ketchup/Sped3.cs
@@ -0,0 +1,56 @@
+﻿using Ketchup.Api;
+
+namespace Ketchup
+{
+    public sealed class Sped3 : PartModule, IDevice
+    {
+        #region Instance Fields
+
+        private IDcpu16 _dcpu16;
+
+        #endregion
+
+        #region Device Identifiers
+
+        public string FriendlyName
+        {
+            get { return "Mackapar Suspended Particle Exciter Display, Rev 3 (SPED-3)"; }
+        }
+
+        public uint ManufacturerId
+        {
+            get { return (uint)Constants.ManufacturerId.Mackapar; }
+        }
+
+        public uint DeviceId
+        {
+            get { return (uint)Constants.DeviceId.Sped3; }
+        }
+
+        public ushort Version
+        {
+            get { return 0x0003; }
+        }
+
+        #endregion
+
+        #region IDevice Methods
+
+        public void OnConnect(IDcpu16 dcpu16)
+        {
+            _dcpu16 = dcpu16;
+        }
+
+        public void OnDisconnect()
+        {
+            _dcpu16 = null;
+        }
+
+        public int OnInterrupt()
+        {
+            return 0;
+        }
+
+        #endregion
+    }
+}
