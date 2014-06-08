6353ff1fcd989e26389afe3b01cdfc6f4e54a228
Dwayne Bent <dbb@dbb.io>
2014-06-08 15:22:01 -0400

    Add Init Target

--- build.fsx
+++ build.fsx
@@ -1,8 +1,21 @@
 #r "packages/FAKE/tools/FakeLib.dll"
 open Fake
 
+// Properties
+let kspDir = "C:/Program Files (x86)/Steam/SteamApps/common/Kerbal Space Program"
+let kspDepDir = kspDir + "/KSP_Data/Managed"
+
+// Targets
 Target "Default" (fun _ ->
     trace "Hello World"
 )
+
+Target "Init" (fun _ ->
+    CreateDir "./Dependencies/KSP"
+    Copy "./Dependencies/KSP" [kspDepDir + "/Assembly-CSharp.dll"; kspDepDir + "/UnityEngine.dll"]
+)
+
+"Init"
+    ==> "Default"
 
 RunTargetOrDefault "Default"
