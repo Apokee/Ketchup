1d85fbdc8d27b048a0695ac334a602b377f87c34
Dwayne Bent <dbb@dbb.io>
2014-06-28 18:53:55 -0400

    Add CRASH device skeleton

---
+++ Source/Ketchup.CrashDevice/KetchupCrashModule.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Ketchup.CrashDevice
+{
+    internal sealed class KetchupCrashModule : PartModule, IDevice
+    {
+        #region Device Identifiers
+
+        public string FriendlyName
+        {
+            get { return "KSG CRASH Controller"; }
+        }
+
+        public uint ManufacturerId
+        {
+            get { return 0xcae02013; } // TODO: Replace with constant
+        }
+
+        public uint DeviceId
+        {
+            get { return 0xcae10001; }
+        }
+
+        public ushort Version
+        {
+            get { return 0x0001; }
+        }
+
+        #endregion
+
+        private IDcpu16 _dcpu16;
+
+        public void OnConnect(IDcpu16 dcpu16)
+        {
+            _dcpu16 = dcpu16;
+        }
+
+        public void OnDisconnect()
+        {
+            _dcpu16 = null;
+        }
+
+        public int OnInterrupt()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
