3efc6bc151412421a0aa99378270a0377efd87ed
Dwayne Bent <dbb@dbb.io>
2014-07-22 22:20:49 -0400

    Dynamically retrieve FAKE version

--- build.ps1
+++ build.ps1
@@ -1,11 +1,12 @@
 # Globals
-$FakeVersion        = "2.18.2"
 $RootDir            = "$PSScriptRoot"
 $NugetDir           = "$RootDir/.nuget"
 $NugetExe           = "$NugetDir/NuGet.exe"
 $PackagesConfigFile = "$NugetDir/packages.config"
 $DependenciesDir    = "$RootDir/Dependencies"
 $PackagesDir        = "$DependenciesDir/NuGet"
+$FakeVersionXPath   = "//package[@id='FAKE'][1]/@version"
+$FakeVersion        = (Select-Xml -Xml ([xml](Get-Content $PackagesConfigFile)) -XPath $FakeVersionXPath).Node.Value
 $FakeExe            = "$PackagesDir/FAKE.$FakeVersion/tools/FAKE.exe"
 
 # Install build packages
