465ab75fdc6d866b01bfd4efb98aacd3eb469e9e
Dwayne Bent <dbb@dbb.io>
2014-07-22 21:45:29 -0400

    Revert build.ps1 back

--- build.ps1
+++ build.ps1
@@ -8,47 +8,9 @@ $DependenciesDir    = "$RootDir/Dependencies"
 $PackagesDir        = "$DependenciesDir/NuGet"
 $FakeExe            = "$PackagesDir/FAKE.$FakeVersion/tools/FAKE.exe"
 
-function Invoke-NugetInstall {
-    Param(
-        [Parameter(Mandatory=$true,Position=1)]
-        [string]$PackageOrConfig,
-
-        [string]$Version,
-        [string]$OutputDirectory,
-
-        [Switch]
-        $ExcludeVersion
-    )
-
-    $arguments = @()
-    $arguments += "install"
-    $arguments += $PackageOrConfig
-    if ($Version) {
-        $arguments += "-Version $Version"
-    }    
-    if ($OutputDirectory) {
-        $arguments += "-OutputDirectory $OutputDirectory"
-    }
-    if ($ExcludeVersion) {
-        $arguments += "-ExcludeVersion"
-    }
-
-    $nuget = start -PassThru -Wait -NoNewWindow $NugetExe $arguments
-    
-    return $nuget.ExitCode
-}
-
-# Install solution packages
-$exitCode = (Invoke-NugetInstall $PackagesConfigFile -OutputDirectory $PackagesDir)
-if ($exitCode -ne 0) {
-    throw "Installing solution packages failed"
-}
+# Install build packages
+iex "$NugetExe install `"$PackagesConfigFile`" -OutputDirectory `"$PackagesDir`""
 
 # Run FAKE
-if ($args) {
-    $fake = start -PassThru -Wait -NoNewWindow $FakeExe $args
-    exit $fake.ExitCode
-} else {
-    $fake = start -PassThru -Wait -NoNewWindow $FakeExe
-    exit $fake.ExitCode
-}
+iex "$FakeExe $args"
+exit $LASTEXITCODE
