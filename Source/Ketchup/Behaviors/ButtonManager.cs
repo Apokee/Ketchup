53f16bc33d85942bd9c743060632731614cd9fff
Dwayne Bent <dbb@dbb.io>
2014-07-24 15:14:16 -0400

    Dynamically show the button

--- Source/Ketchup/Behaviors/ButtonManager.cs
+++ Source/Ketchup/Behaviors/ButtonManager.cs
@@ -1,5 +1,8 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Reflection;
+using Ketchup.Modules;
 using Ketchup.Services;
 using UnityEngine;
 
@@ -13,6 +16,10 @@ namespace Ketchup.Behaviors
 
         private static Texture2D _texture;
         private ApplicationLauncherButton _button;
+        private bool _canShowButton;
+
+        private EditorLogic _editorLogic;
+        private int _editorPartCount;
 
         public void Awake()
         {
@@ -33,12 +40,26 @@ namespace Ketchup.Behaviors
 
         public void Start()
         {
+            if (HighLogic.LoadedSceneIsEditor)
+            {
+                _editorLogic = EditorLogic.fetch;
+            }
+
             GameEvents.onGUIApplicationLauncherReady.Add(OnGuiApplicationLauncherReady);
             GameEvents.onGameSceneLoadRequested.Add(OnGameSceneLoadRequested);
+            GameEvents.onPartAttach.Add(OnPartAttach);
+            GameEvents.onPartRemove.Add(OnPartRemove);
+        }
+
+        public void Update()
+        {
+            EditorCheckForFirstPart();
         }
 
         public void OnDestroy()
         {
+            GameEvents.onPartRemove.Remove(OnPartRemove);
+            GameEvents.onPartAttach.Remove(OnPartAttach);
             GameEvents.onGameSceneLoadRequested.Remove(OnGameSceneLoadRequested);
             GameEvents.onGUIApplicationLauncherReady.Remove(OnGuiApplicationLauncherReady);
         }
@@ -49,16 +70,9 @@ namespace Ketchup.Behaviors
             {
                 Log(LogLevel.Debug, "Adding AppLauncher button");
 
-                _button = ApplicationLauncher.Instance.AddModApplication(
-                    OnButtonTrue,
-                    OnButtonFalse,
-                    OnButtonHover,
-                    OnButtonHoverOut,
-                    OnEnable,
-                    OnDisable,
-                    ButtonScenes,
-                    _texture
-                );
+                _canShowButton = true;
+
+                UpdateButtonState(checkParts: false);
             }
         }
 
@@ -66,10 +80,24 @@ namespace Ketchup.Behaviors
         {
             if (_button != null)
             {
-                Log(LogLevel.Debug, "Removing AppLauncher button");
+                _canShowButton = false;
 
-                ApplicationLauncher.Instance.RemoveModApplication(_button);
+                UpdateButtonState(checkParts: false);
             }
+        }
+
+        private void OnPartAttach(GameEvents.HostTargetAction<Part, Part> data)
+        {
+            Log(LogLevel.Debug, "OnPartAttach()");
+
+            UpdateButtonState(checkParts: true);
+        }
+
+        private void OnPartRemove(GameEvents.HostTargetAction<Part, Part> data)
+        {
+            Log(LogLevel.Debug, "OnPartRemove()");
+
+            UpdateButtonState(checkParts: true);
         }
 
         private void OnButtonTrue()
@@ -92,14 +120,92 @@ namespace Ketchup.Behaviors
             Log(LogLevel.Debug, "OnButtonHoverOut()");
         }
 
-        private void OnEnable()
+        private void OnButtonEnable()
         {
             Log(LogLevel.Debug, "OnEnable()");
         }
 
-        private void OnDisable()
+        private void OnButtonDisable()
         {
             Log(LogLevel.Debug, "OnDisable()");
+        }
+
+        private void EditorCheckForFirstPart()
+        {
+            if (HighLogic.LoadedSceneIsEditor)
+            {
+                if (_editorLogic != null)
+                {
+                    var oldPartCount = _editorPartCount;
+                    var newPartCount = _editorLogic.ship.Count;
+
+                    if (oldPartCount == 0 && newPartCount > 0)
+                    {
+                        Log(LogLevel.Debug, "First Part(s) Added");
+
+                        UpdateButtonState(checkParts: true);
+                    }
+
+                    if (oldPartCount > 0 && newPartCount == 0)
+                    {
+                        Log(LogLevel.Debug, "Last Part(s) Removed");
+
+                        UpdateButtonState(checkParts: true);
+                    }
+
+                    _editorPartCount = newPartCount;
+                }
+            }
+        }
+
+        private void UpdateButtonState(bool checkParts)
+        {
+            if (_canShowButton && checkParts)
+            {
+                IEnumerable<Part> parts = null;
+
+                if (_editorLogic != null)
+                {
+                    parts = _editorLogic.ship.Parts;
+                }
+
+                if (parts != null)
+                {
+                    if (ContainsComputer(parts))
+                    {
+                        if (_button == null)
+                        {
+                            _button = ApplicationLauncher.Instance.AddModApplication(
+                                OnButtonTrue,
+                                OnButtonFalse,
+                                OnButtonHover,
+                                OnButtonHoverOut,
+                                OnButtonEnable,
+                                OnButtonDisable,
+                                ButtonScenes,
+                                _texture
+                            );
+                        }
+                    }
+                    else
+                    {
+                        if (_button != null)
+                        {
+                            ApplicationLauncher.Instance.RemoveModApplication(_button);
+                        }
+                    }
+                }
+            }
+
+            if (_button != null && !_canShowButton)
+            {
+                ApplicationLauncher.Instance.RemoveModApplication(_button);
+            }
+        }
+
+        private static bool ContainsComputer(IEnumerable<Part> parts)
+        {
+            return parts.SelectMany(i => i.FindModulesImplementing<ModuleKetchupComputer>()).Any();
         }
 
         private static void Log(LogLevel level, string message, params object[] args)
