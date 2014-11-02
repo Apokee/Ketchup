ea51d39499e7a5d2fcb4031ccd484a36da9fb60b
Dwayne Bent <dbb@dbb.io>
2014-11-02 15:23:47 -0500

    Implement basic methods and properties

--- Source/Ketchup/Modules/ModuleKetchupGlobalController.cs
+++ Source/Ketchup/Modules/ModuleKetchupGlobalController.cs
@@ -8,50 +8,26 @@ namespace Ketchup.Modules
     {
         #region Device Identifiers
 
-        public string FriendlyName
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public uint ManufacturerId
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public uint DeviceId
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public ushort Version
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public Port Port
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-            set
-            {
-                throw new NotImplementedException();
-            }
-        }
+        public string FriendlyName { get { return "Global Controller"; } }
+        public uint ManufacturerId { get { return (uint)Constants.ManufacturerId.Apokee; } }
+        public uint DeviceId { get { return 0x73B0AD20; } } // FIXME: Original DeviceId was too long
+        public ushort Version { get { return 0x0001; } }
+        public Port Port { get; set; }
 
         #endregion
+
+        private IDcpu16 _dcpu16;
 
         #region IDevice
 
         public void OnConnect(IDcpu16 dcpu16)
         {
-            throw new NotImplementedException();
+            _dcpu16 = dcpu16;
         }
 
         public void OnDisconnect()
         {
-            throw new NotImplementedException();
+            _dcpu16 = null;
         }
 
         public int OnInterrupt()
