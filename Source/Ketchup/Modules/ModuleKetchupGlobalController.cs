7b96200ef6b483697f4d6e27ff54fdbdef8beeaa
Dwayne Bent <dbb@dbb.io>
2014-11-02 14:30:46 -0500

    Add Module for Global Controller

---
+++ Source/Ketchup/Modules/ModuleKetchupGlobalController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Ketchup.Api.v0;
+
+namespace Ketchup.Modules
+{
+    [KSPModule("Device: Global Controller")]
+    internal sealed class ModuleKetchupGlobalController : PartModule, IDevice
+    {
+        #region Device Identifiers
+
+        public string FriendlyName
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public uint ManufacturerId
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public uint DeviceId
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public ushort Version
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public Port Port
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        #endregion
+
+        #region IDevice
+
+        public void OnConnect(IDcpu16 dcpu16)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnDisconnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int OnInterrupt()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
