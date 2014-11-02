1f5d82cdfd6438d95947b5187980090f759460b4
Dwayne Bent <dbb@dbb.io>
2014-11-02 18:39:45 -0500

    Add TODOs

--- Source/Ketchup/Modules/ModuleKetchupGlobalController.cs
+++ Source/Ketchup/Modules/ModuleKetchupGlobalController.cs
@@ -57,10 +57,10 @@ namespace Ketchup.Modules
                 case 0x0005:
                     // FIXME: The total number of stages in a rocket isn't really defined.
                     break;
-                case 0x0006:
+                case 0x0006: // TODO: Test
                     _dcpu16.A = (ushort)vessel.currentStage;
                     break;
-                case 0x0007:
+                case 0x0007: // TODO: Test
                     if (vessel.altitude >= 0)
                     {
                         _dcpu16.A = (ushort)(((uint)vessel.altitude) >> 16);
@@ -70,7 +70,7 @@ namespace Ketchup.Modules
                         // FIXME: Undefined when altitude is negative (below sea-level)
                     }
                     break;
-                case 0x0008:
+                case 0x0008: // TODO: Test
                     if (vessel.altitude >= 0)
                     {
                         _dcpu16.A = (ushort)(((uint)vessel.altitude) & 0x000011116);
@@ -80,7 +80,7 @@ namespace Ketchup.Modules
                         // FIXME: Undefined when altitude is negative (below sea-level)
                     }
                     break;
-                case 0x0009:
+                case 0x0009: // TODO: Test
                     if (vessel.terrainAltitude >= 0)
                     {
                         if (vessel.terrainAltitude <= UInt16.MaxValue)
@@ -97,7 +97,7 @@ namespace Ketchup.Modules
                         // FIXME: Undefined when terrain altitude is negative (below water)
                     }
                     break;
-                case 0x000a:
+                case 0x000a: // TODO: Test
                     _dcpu16.A = FixedPoint.Convert(vessel.staticPressure);
                     break;
             }
